de3f82c2594c8683ca581a1c3df817d262963fb2
ozzie <ocarreon@appdev.yearup.org>
2025-04-13 21:40:10 -0400

    switch inlcuding second calculator

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -7,14 +7,26 @@ public class Main {
     public static final int MONTHS_IN_YEAR = 12; //
     // this is your constant it does not change throughout the code
     // also serves as a reminder rather than just using the number 12(represents months in a year)
+    public static final int DAYS_IN_YEAR = 365;
 
     public static void main(String[] args) {
-        Scanner scanner   = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("1) Mortgage Calculator");
         System.out.println("2) Future Value Calculator");
 
         int givenCommand = scanner.nextInt();
+
+        switch (givenCommand) {
+            case 1:
+                calculateMortgage(scanner);
+                break;
+            case 2:
+                calculateFutureValue(scanner);
+                break;
+        }
+    }
+    public static void calculateMortgage(Scanner scanner) {
 
         System.out.println("Enter the principal amount:");
         double principal = scanner.nextDouble();
@@ -28,10 +40,37 @@ public class Main {
         float monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR;
         int numberOfPayments = termInYears * MONTHS_IN_YEAR;
 
-        double monthlyPayment = principal * (monthlyInterestRate * (Math.pow(1 + monthlyInterestRate, numberOfPayments))) / ((Math.pow(1 + monthlyInterestRate, numberOfPayments))-1);
+        double monthlyPayment = principal * (monthlyInterestRate * (Math.pow(1 + monthlyInterestRate, numberOfPayments))) / ((Math.pow(1 + monthlyInterestRate, numberOfPayments)) - 1);
 
         System.out.println("Monthly payment: " + monthlyPayment);
         System.out.println("Total interest amount: " + ((monthlyPayment * numberOfPayments) - principal));
+    }
+    public static void calculateFutureValue(Scanner scanner) {
+        System.out.println("Enter the deposit amount:");
+        double deposit = scanner.nextDouble();
+
+        System.out.println("Enter the interest rate:");
+        float annualInterestRate = scanner.nextFloat();
+
+        System.out.println("Enter the number of years");
+        int numberOfYears = scanner.nextInt();
+
+
+
+        double futureValue = deposit * Math.pow(1 + annualInterestRate / DAYS_IN_YEAR, DAYS_IN_YEAR * numberOfYears);
+        double interestTotal = futureValue - deposit;
+
+        System.out.printf("Your future value will be: $%.3f%n", futureValue);
+        System.out.printf("Your total interest will be: $%.3f%n", interestTotal);
+
+
+
+
+
+
+
+
+
 
 
 
